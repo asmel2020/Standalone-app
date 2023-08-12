2e78fa99d78ace273c7a9c7af0ac7369ca1f4239
Danny Gonzalez <danny.galicia@rhizom.me>
2023-08-12 09:38:16 -0300

    feat: add init main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AppService } from './app.service';
+
 
 async function bootstrap() {
-
   const app = await NestFactory.createApplicationContext(AppModule);
-  
   await app.init();
 }
+
 bootstrap();
